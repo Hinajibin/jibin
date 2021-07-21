3a68a3e73ac3d3cf2f2e9c8affb563154d6d6a9f
Hinajibin <87356174+Hinajibin@users.noreply.github.com>
2021-07-21 11:52:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} /n @mjmoviesrequesta",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰MAIN CHANNEL🔰', url="https://t.me/mjmoviesofficial"
                                 )
                         ]
                     ]
